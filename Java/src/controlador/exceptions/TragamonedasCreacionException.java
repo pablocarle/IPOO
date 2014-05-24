bf4e256334051a96b2de2ef349e6adb42448d457
Pablo <pablo.carle@gmail.com>
2014-05-24 20:51:19 -0300

    Cambios luego de conferencia 24.05.2014

---
+++ Java/src/controlador/exceptions/TragamonedasCreacionException.java
@@ -0,0 +1,36 @@
+package controlador.exceptions;
+
+
+public class TragamonedasCreacionException extends TragamonedasException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TragamonedasCreacionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TragamonedasCreacionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TragamonedasCreacionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TragamonedasCreacionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TragamonedasCreacionException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
