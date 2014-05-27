f47a568939e85f92f8b6838973ada22b4f93f768
Pablo <pablo.carle@gmail.com>
2014-05-27 19:24:05 -0300

    Cambios en AltaPremioForm
    Distribuido en initGUI e initEvents, agregado boton de limpiar textArea

--- Java/src/vista/config/AltaPremioForm.java
+++ Java/src/vista/config/AltaPremioForm.java
@@ -14,6 +14,7 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import vista.MainFrame;
+import vista.UserMessageView;
 import controlador.Sistema;
 
 public class AltaPremioForm extends JPanel {
@@ -32,6 +33,7 @@ public class AltaPremioForm extends JPanel {
 	private JButton btnSandia;
 	private JButton btnAceptar;
 	private JButton btnCancelar;
+	private JButton btnLimpiar;
 	private Sistema sistema;
 
 
@@ -39,127 +41,157 @@ public class AltaPremioForm extends JPanel {
 		super();
 		initGUI();
 		this.sistema = sistema;
+		
 	}
 
 	public void initGUI() {
-		try {
-			setLayout(null);
+	
+		setLayout(null);
 
-			JLabel lblnroMaquina = new JLabel("Nro Tragamonedas");
-			lblnroMaquina.setBounds(12, 30, 202, 15);
-			add(lblnroMaquina);
+		JLabel lblnroMaquina = new JLabel("Nro Tragamonedas");
+		lblnroMaquina.setBounds(12, 30, 202, 15);
+		add(lblnroMaquina);
 
-			JLabel lblvalorPremio = new JLabel("Valor del Premio");
-			lblvalorPremio.setBounds(220, 30, 202, 15);
-			add(lblvalorPremio);
+		JLabel lblvalorPremio = new JLabel("Valor del Premio");
+		lblvalorPremio.setBounds(220, 30, 202, 15);
+		add(lblvalorPremio);
 
-			combinacionText=new JTextArea();
-			combinacionText.setBounds(80,70,300,100);
+		combinacionText = new JTextArea();
+		combinacionText.setBounds(80,70,300,100);
 //			Deshabilitamos para que solo puedan cargarse frutas con los botones
-			combinacionText.setEnabled(false);
-			add(combinacionText);
+		combinacionText.setEnabled(false);
+		add(combinacionText);
 
-			valorPremioText = new JTextField();
-			valorPremioText.setBounds(324, 28, 114, 19);
-			add(valorPremioText);
-			valorPremioText.setColumns(10);
+		valorPremioText = new JTextField();
+		valorPremioText.setBounds(324, 28, 114, 19);
+		add(valorPremioText);
+		valorPremioText.setColumns(10);
 
-			nroMaquinaText = new JTextField();
-			nroMaquinaText.setBounds(140, 28, 20, 19);
-			add(nroMaquinaText);
-			nroMaquinaText.setColumns(10);
+		nroMaquinaText = new JTextField();
+		nroMaquinaText.setBounds(140, 28, 20, 19);
+		add(nroMaquinaText);
+		nroMaquinaText.setColumns(10);
 
-			btnBanana = new JButton("Banana");
-			//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
-			btnBanana.addActionListener(new ActionListener() {
+		btnBanana = new JButton("Banana");
+		//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
+		btnBanana.setBounds(10, 200, 80, 25);
+		add(btnBanana);
 
-				public void actionPerformed(ActionEvent arg0) {
-					combinacionText.append("Banana\n");
-					combinacionText.setLineWrap(true);
-				}
-			});	
-			btnBanana.setBounds(10, 200, 80, 25);
-			add(btnBanana);
+		btnFrutilla = new JButton("Frutilla");
+		//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg"));
+		btnFrutilla.setBounds(100, 200, 80, 25);
+		add(btnFrutilla);
 
-			btnFrutilla = new JButton("Frutilla");
-			//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg"));
-			btnFrutilla.addActionListener(new ActionListener() {
+		btnManzana = new JButton("Manzana");
+		//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
+		btnManzana.setBounds(190, 200, 80, 25);
+		add(btnManzana);
 
-				public void actionPerformed(ActionEvent arg0) {
-					combinacionText.append("Frutilla\n");
-					combinacionText.setLineWrap(true);
-				}
-			});	
-			btnFrutilla.setBounds(100, 200, 80, 25);
-			add(btnFrutilla);
+		btnPera = new JButton("Pera");
+		//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
+		btnPera.setBounds(280, 200, 80, 25);
+		add(btnPera);
 
-			btnManzana = new JButton("Manzana");
-			//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
-			btnManzana.addActionListener(new ActionListener() {
+		btnSandia = new JButton("Sandia");
+		//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
+		btnSandia.setBounds(370, 200, 80, 25);
+		add(btnSandia);
 
-				public void actionPerformed(ActionEvent arg0) {
-					combinacionText.append("Manzana\n");
-					combinacionText.setLineWrap(true);
-				}
-			});	
-			btnManzana.setBounds(190, 200, 80, 25);
-			add(btnManzana);
+		btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBounds(100, 300, 117, 25);
+		add(btnAceptar);
 
-			btnPera = new JButton("Pera");
-			//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
-			btnPera.addActionListener(new ActionListener() {
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(242, 300, 117, 25);
+		add(btnCancelar);
+		setSize(500, 400);
+		
+		btnLimpiar = new JButton("Limpiar");
+		btnLimpiar.setBounds(390, 107, 89, 23);
+		add(btnLimpiar);
+		
+		initEvents();
 
-				public void actionPerformed(ActionEvent arg0) {
-					combinacionText.append("Pera\n");
-					combinacionText.setLineWrap(true);
-				}
-			});	
-			btnPera.setBounds(280, 200, 80, 25);
-			add(btnPera);
+	}
+	
+	private void initEvents() {
+		btnCancelar.addActionListener(new ActionListener() {
 
-			btnSandia = new JButton("Sandia");
-			//btnBanana.setIcon(new javax.swing.ImageIcon("banana.jpg")); 
-			btnSandia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+//				Cerrar la ventana y volver al menu principal
+				getParent().setVisible(false);
+				JFrame mainPage = new MainFrame();
+				mainPage.setVisible(true);
+			}
+		});
+		
+		btnAceptar.addActionListener(new ActionListener() {
 
-				public void actionPerformed(ActionEvent arg0) {
-					combinacionText.append("Sandia\n");
-					combinacionText.setLineWrap(true);
-				}
-			});	
-			btnSandia.setBounds(370, 200, 80, 25);
-			add(btnSandia);
-
-			btnAceptar = new JButton("Aceptar");
-			btnAceptar.addActionListener(new ActionListener() {
-
-				public void actionPerformed(ActionEvent arg0) {
-					float valorPremio = Float.parseFloat(valorPremioText.getText());
-					int Nromaquina = Integer.parseInt(nroMaquinaText.getText());
+			public void actionPerformed(ActionEvent arg0) {
+				float valorPremio;
+				int Nromaquina;
+				List<String> combinacionPremio;
+				try {
+					valorPremio = Float.parseFloat(valorPremioText.getText());
+					Nromaquina = Integer.parseInt(nroMaquinaText.getText());
 					String[] texto = combinacionText.getText().split("\n");
-					List<String> combinacionPremio = Arrays.asList(texto);
-
-					sistema.altaPremio(Nromaquina, combinacionPremio, valorPremio);
+					combinacionPremio = Arrays.asList(texto);
+					
+					UserMessageView vistaRetorno = sistema.altaPremio(Nromaquina, combinacionPremio, valorPremio);
+					
+					JOptionPane.showMessageDialog(null, vistaRetorno.getMensaje());
+				} catch (NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Ocurrio un error al dar de alta el premio, revise los datos", "Error", JOptionPane.ERROR_MESSAGE);
 				}
-			});	
-			btnAceptar.setBounds(100, 300, 117, 25);
-			add(btnAceptar);
+			}
+		});	
+		
+		btnLimpiar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				combinacionText.setText("");				
+			}
+		});
+		
+		btnSandia.addActionListener(new ActionListener() {
 
-			btnCancelar = new JButton("Cancelar");
-			btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				combinacionText.append("Sandia\n");
+				combinacionText.setLineWrap(true);
+			}
+		});	
+		
+		btnBanana.addActionListener(new ActionListener() {
 
-				public void actionPerformed(ActionEvent arg0) {
-					//						Cerrar la ventana y volver al menu principal
-					getParent().setVisible(false);
-					JFrame mainPage = new MainFrame();
-					mainPage.setVisible(true);
-				}
-			});	
-			btnCancelar.setBounds(242, 300, 117, 25);
-			add(btnCancelar);
-			setSize(500, 400);
-		} catch (Exception e) {
-			JOptionPane.showMessageDialog(null, "Ocurrio un error al dar de alta el premio, revise los datos", "Error", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-		}
+			public void actionPerformed(ActionEvent arg0) {
+				combinacionText.append("Banana\n");
+				combinacionText.setLineWrap(true);
+			}
+		});	
+
+		btnFrutilla.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				combinacionText.append("Frutilla\n");
+				combinacionText.setLineWrap(true);
+			}
+		});	
+
+		btnPera.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				combinacionText.append("Pera\n");
+				combinacionText.setLineWrap(true);
+			}
+		});	
+		
+		btnManzana.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				combinacionText.append("Manzana\n");
+				combinacionText.setLineWrap(true);
+			}
+		});	
 	}
 }
