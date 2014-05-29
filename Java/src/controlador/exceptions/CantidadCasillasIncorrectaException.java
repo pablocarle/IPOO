d88bbf5480febbfd754ebd183d9355ddd95cef31
fguerreroUADE <fguerrero2@gmail.com>
2014-05-28 21:19:22 -0300

    (Francisco) UML actualizado

---
+++ Java/src/controlador/exceptions/CantidadCasillasIncorrectaException.java
@@ -0,0 +1,29 @@
+package controlador.exceptions;
+
+public class CantidadCasillasIncorrectaException extends PremioException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CantidadCasillasIncorrectaException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CantidadCasillasIncorrectaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CantidadCasillasIncorrectaException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CantidadCasillasIncorrectaException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Java/src/controlador/exceptions/CombinacionExistenteException.java
@@ -0,0 +1,29 @@
+package controlador.exceptions;
+
+public class CombinacionExistenteException extends PremioException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CombinacionExistenteException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CombinacionExistenteException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CombinacionExistenteException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CombinacionExistenteException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
